f5c885ed23eb86a492315a48b408fa4338f05489
Claudio Vega Lagos <claudio.vegal@sansano.usm.cl>
2020-07-20 09:32:56 -0400

    documentos base

---
+++ backend/geph-usm-isw/src/main/java/cl/geph/usm/gephusmisw/GephPlataformApplication.java
@@ -0,0 +1,13 @@
+package cl.geph.usm.gephusmisw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GephPlataformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GephPlataformApplication.class, args);
+	}
+
+}
---
+++ backend/geph-usm-isw/src/test/java/cl/geph/usm/gephusmisw/GephPlataformApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.geph.usm.gephusmisw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GephPlataformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
