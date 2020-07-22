5cb68838f6e22d466d79230188b1d2625bce7c7f
Claudio Vega Lagos <claudio.vegal@sansano.usm.cl>
2020-07-21 20:42:11 -0400

    new config

---
+++ backend/geph/src/main/java/cl/geph/usm/GephApplication.java
@@ -0,0 +1,13 @@
+package cl.geph.usm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GephApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GephApplication.class, args);
+	}
+
+}
---
+++ backend/geph/src/test/java/cl/geph/usm/GephApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.geph.usm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GephApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
